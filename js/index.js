0c0504451d0be4da87d556b731869cb01aefbc2e
joshuansu <joshuansu0897@gmail.com>
2017-09-27 18:19:47 -0700

    ya quedo casi todo, faltaria cambiar alguna info y teminar el conacto

--- js/index.js
+++ js/index.js
@@ -62,3 +62,29 @@ function scaleBannerVideoSize(element){
 
     });
 }
+
+
+(function () {
+
+    var Pagina = function () {
+
+        function init() {
+            initFlip();
+        }
+
+        function initFlip() {
+
+          $(".flip-container a").click(function () {
+            if ($(this).closest(".flip-container").hasClass("rotate")) { $(this).closest(".flip-container").removeClass("rotate"); }
+            else { $(this).closest(".flip-container").addClass("rotate"); }
+          });
+        }
+
+        init();
+    };
+
+    $(document).ready(function () {
+        new Pagina();
+    });
+
+})();
